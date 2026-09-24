b16431636df9266a5772fbb6e3704158fd5d647c
Patrick McDonald <paddymcdonald@gmail.com>
2026-09-24 23:58:16 +0100

    feat: Add Service Bus

--- DemoFunctions.AppHost/AppHost.cs
+++ DemoFunctions.AppHost/AppHost.cs
@@ -1,5 +1,22 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddAzureFunctionsProject<Projects.DemoFunctions>("demofunctions");
+var serviceBus = builder.AddAzureServiceBus("messaging")
+    .RunAsEmulator(e => e
+        .WithHostPort(56721)
+        .WithEndpoint("emulatorhealth", url =>
+        {
+            url.Port = 53001;
+        }));
 
-builder.Build().Run();
+var topic = serviceBus.AddServiceBusTopic("topic", "az.topic");
+
+var sub1 = topic.AddServiceBusSubscription("sub1", "az.sub1");
+var sub2 = topic.AddServiceBusSubscription("sub2", "az.sub2");
+
+builder.AddAzureFunctionsProject<Projects.DemoFunctions>("demofunctions")
+    .WithReference(serviceBus)
+    .WaitFor(sub1)
+    .WaitFor(sub2)
+    .WithEnvironment("ServiceBus:ConnectionString", serviceBus);
+
+await builder.Build().RunAsync();
